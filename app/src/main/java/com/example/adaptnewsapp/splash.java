bb467e52d7ead0c3c45ae01556a23bdcf2f7dd65
BoredPotatoDev <martinjoshuaddetona@iskolarngbayan.pup.edu.ph>
2022-07-27 02:12:27 +0800

    icon update

---
+++ app/src/main/java/com/example/adaptnewsapp/splash.java
@@ -0,0 +1,29 @@
+package com.example.adaptnewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
