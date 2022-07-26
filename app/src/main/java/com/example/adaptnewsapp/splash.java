9d9a2f688be010a8870209289bce10a357bb5583
BoredPotatoDev <martinjoshuaddetona@iskolarngbayan.pup.edu.ph>
2022-07-27 03:11:58 +0800

    Splash Screen fix and minor tweaks

--- app/src/main/java/com/example/adaptnewsapp/splash.java
+++ app/src/main/java/com/example/adaptnewsapp/splash.java
@@ -5,25 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 
 public class splash extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        Handler handler;
+        handler= new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent=new Intent(splash.this,MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }, 1100);
     }
 }
